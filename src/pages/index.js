8d6a7fa0fde5e604ca64393461536041f1c45cc1
Adam Fratino <fratino.a@gmail.com>
2023-02-19 12:52:55 -0500

    minor page stuff

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+const Homepage = () => <div>Homepage</div>
+
+export default Homepage;
+
+// Page.getInitialProps = async ({ query }) => {
+//   const res = await fetch(
+//     `https://api.challonge.com/v1/tournaments/brooklyn-shuffleboard-singles3/participants.json?api_key=${process.env.CHALLONGE_API_KEY}`
+//   );
+//   const data = await res.json();
+//   return { data: data };
+// };
